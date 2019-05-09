19a9d62fcb9ddef5f6b4b6c204d4407c9abb7192
laurent.pichler <laurent.pichler@sealsystems.de>
2019-05-09 16:32:12 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# node-determine-executable-version
+# @sealsystems/determine-executable-version
+[![CircleCI](https://circleci.com/gh/sealsystems/node-determine-executable-version.svg?style=svg)](https://circleci.com/gh/sealsystems/node-determine-executable-version)
+
+Calls an executable and returns the version information it writes to stdout.
+
+# Installation
+
+```bash
+$ npm install seal-determine-executable-version
+```
+
+# Usage
+
+```javascript
+const determineExecutableVersion = require('seal-determine-executable-version');
+const options = {
+  exeFile: '/bin/bash',
+  maxBytes: 128,
+  arguments: ['--version']
+  };
+
+const versionString = await determineExecutableVersion(options);
+
+
+```
+
+Parameters:
+
+- ```exeFile``` The full path to the executable file whose version shall be determined. Required option.
+- ```maxBytes``` The maximum number of bytes to return from the output; output which is longer is truncated. Default: ```128```
+- ```arguments``` The argument list to call the executable file with. Default: ```['--version']```
+
+## Running the build
+
+To build this module use [roboter](https://www.npmjs.com/package/roboter).
+
+```shell
+$ bot
+```
---
+++ test/determineExecutableVersionTest.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const assert = require('assertthat');
+const determineExecutableVersion = require('../lib/index');
+
+suite('determineExecutableVersion', () => {
+  test('is a function', (done) => {
+    assert.that(determineExecutableVersion).is.ofType('function');
+    done();
+  });
+
+  test('errors if given no exeFile to execute', async () => {
+    await assert.that(async () => {
+      await determineExecutableVersion({ something: true });
+    }).is.throwingAsync();
+  });
+
+  test('errors if the exeFile cannot be spawned', async () => {
+    await assert.that(async () => {
+      await determineExecutableVersion({ exeFile: 'nonexistentBinary123' });
+    }).is.throwingAsync();
+  });
+
+  test('errors if maxBytes is not of type number', async () => {
+    await assert.that(async () => {
+      await determineExecutableVersion({ exeFile: process.argv[0], maxBytes: false });
+    }).is.throwingAsync();
+  });
+
+  test('errors if exeArguments is not an array', async () => {
+    await assert.that(async () => {
+      await determineExecutableVersion({ exeFile: process.argv[0], exeArguments: '--version' });
+    }).is.throwingAsync();
+  });
+
+  test('truncates output to maxBytes length', async () => {
+    const nodeOutput = await determineExecutableVersion({ exeFile: process.argv[0], maxBytes: 2 });
+
+    assert.that(nodeOutput.length).is.equalTo(2);
+  });
+});
